07bcd87917d7a0742faba9163051cd7d6064d6d2
Gabor <ghalasz1988@gmail.com>
2021-05-07 21:09:29 +0100

    regex sanitised, partial matches included

--- src/App.js
+++ src/App.js
@@ -15,23 +15,52 @@ const App = () => {
     setUploadedImgs(pictureURL);
   };
 
+  const sanitize = (text) => {
+    console.log(text);
+    let sanitizedText =
+      text.substr(0, 2) +
+      "°" +
+      text.substr(3, 2) +
+      '"' +
+      "." +
+      text.substr(7, 3) +
+      text.substr(10, 3) +
+      "°" +
+      text.substr(14, 3) +
+      "." +
+      text.substr(18, 3);
+    return sanitizedText;
+  };
+
+  const fullMatchRegex = /[0-9]{2}.[0-9]{2}.{1,2}[0-9]{2}[NS]\s[0-9]{2}.[0-9]{2}.{1,2}[0-9]{2}[WE]/gi;
+  const partialMatchRegex = /[0-9]{2}.[0-9]{2}.{1,2}[0-9]{2}[NS]\s[0-9]{2}.[0-9]{2}/gi;
+
   const handleImageOCR = () => {
     uploadedImgs.forEach((picture) => {
-      console.log(picture);
       Tesseract.recognize(picture, "eng", {
-        logger: (ww) => {
-          setProgress(ww.progress * 100);
+        logger: (worker) => {
+          if (worker.progress * 100 > progress)
+            setProgress(worker.progress * 100);
         },
-      }).then(({ data: { text } }) => {
-        setCoordinates({
-          fullMatches: text.match(
-            /[0-9]{2}.[0-9]{2}.{1,2}[0-9]{2}[NS]\s[0-9]{2}.[0-9]{2}.{1,2}[0-9]{2}[WE]/gi
-          ),
-          partialMatches: text.match(
-            /[0-9]{2}.[0-9]{2}.{1,2}[0-9]{2}[NS]\s[0-9]{2}.[0-9]{2}/gi
-          ),
+      })
+        .then(({ data: { text } }) => {
+          let fullMatches = [];
+          let partialMatches = [];
+          text.match(fullMatchRegex).forEach((match) => {
+            fullMatches.push(sanitize(match));
+          });
+
+          text.match(partialMatchRegex);
+
+          console.log(fullMatches);
+          return { fullMatches, partialMatches };
+        })
+        .then(({ fullMatches, partialMatches }) => {
+          setCoordinates({
+            fullMatches,
+            partialMatches,
+          });
         });
-      });
     });
   };
 
@@ -84,28 +113,26 @@ const App = () => {
         coordinates?.fullMatches.length > 0) && (
         <div className="results-container">
           <ul className="full-matches">
-            {" "}
-            {coordinates.fullMatches.map((matches) => (
+            <strong>{coordinates.fullMatches.length + " full matches"}</strong>
+            {coordinates.fullMatches.map((match) => (
               <li
-                className="coordinate"
-                key={coordinates.fullMatches.indexOf(matches)}
+                className="coordinate-list-element"
+                key={coordinates.fullMatches.indexOf(match)}
               >
-                <strong>{coordinates.fullMatches.indexOf(matches) + 1}</strong>
-                <p>{matches}</p>
+                <p>{match}</p>
               </li>
             ))}
           </ul>
           <ul className="partial-matches">
-            {" "}
-            {coordinates.partialMatches.map((matches) => (
+            <strong>
+              {coordinates.partialMatches.length + " partial matches"}
+            </strong>{" "}
+            {coordinates.partialMatches.map((match) => (
               <li
-                className="coordinate"
-                key={coordinates.partialMatches.indexOf(matches)}
+                className="coordinate-list-element"
+                key={coordinates.partialMatches.indexOf(match)}
               >
-                <strong>
-                  {coordinates.partialMatches.indexOf(matches) + 1}
-                </strong>
-                <p>{matches}</p>
+                <p>{match}</p>
               </li>
             ))}
           </ul>
